bb2d3ae296ffbd636d1f3b625255871909914b6f
itsAishbaa <aalishbaa3@gmail.com>
2026-05-17 07:58:08 -0700

    Adding code in BattleRecord.cs

--- Components/Models/BattleRecord.cs
+++ Components/Models/BattleRecord.cs
@@ -2,5 +2,14 @@
 {
     public class BattleRecord
     {
+        public int Id { get; set; }
+
+        public string Winner { get; set; }
+        public string Loser { get; set; }
+
+        public int KnightHealth { get; set; }
+        public int SkeletonHealth { get; set; }
+
+        public DateTime BattleTime { get; set; } = DateTime.Now;
     }
 }
